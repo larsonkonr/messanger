63ba182d83ce20dec029c953e580175ae8032ade
Konr Larson <larsonkonr@Konrs-MacBook-Pro.local>
2015-04-02 09:18:57 -0600

    schema change

--- db/schema.rb
+++ db/schema.rb
@@ -13,55 +13,30 @@
 
 ActiveRecord::Schema.define(version: 20150324173929) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "conversations", force: :cascade do |t|
     t.integer  "sender_id"
     t.integer  "recipient_id"
-    t.datetime "created_at",   null: false
-    t.datetime "updated_at",   null: false
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
-  add_index "conversations", ["recipient_id"], name: "index_conversations_on_recipient_id"
-  add_index "conversations", ["sender_id"], name: "index_conversations_on_sender_id"
+  add_index "conversations", ["recipient_id"], name: "index_conversations_on_recipient_id", using: :btree
+  add_index "conversations", ["sender_id"], name: "index_conversations_on_sender_id", using: :btree
 
-  create_table "conversations", force: true do |t|
-      t.integer  "sender_id"
-      t.integer  "recipient_id"
-      t.datetime "created_at"
-      t.datetime "updated_at"
-    end
+  create_table "messages", force: :cascade do |t|
+    t.text     "body"
+    t.integer  "conversation_id"
+    t.integer  "user_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
-    add_index "conversations", ["recipient_id"], name: "index_conversations_on_recipient_id"
-    add_index "conversations", ["sender_id"], name: "index_conversations_on_sender_id"
+  add_index "messages", ["conversation_id"], name: "index_messages_on_conversation_id", using: :btree
+  add_index "messages", ["user_id"], name: "index_messages_on_user_id", using: :btree
 
-    create_table "messages", force: true do |t|
-      t.text     "body"
-      t.integer  "conversation_id"
-      t.integer  "user_id"
-      t.datetime "created_at"
-      t.datetime "updated_at"
-    end
-
-    add_index "messages", ["conversation_id"], name: "index_messages_on_conversation_id"
-    add_index "messages", ["user_id"], name: "index_messages_on_user_id"
-
-    # create_table "users", force: true do |t|
-    #   t.string   "email",                  default: "", null: false
-    #   t.string   "encrypted_password",     default: "", null: false
-    #   t.string   "reset_password_token"
-    #   t.datetime "reset_password_sent_at"
-    #   t.datetime "remember_created_at"
-    #   t.integer  "sign_in_count",          default: 0,  null: false
-    #   t.datetime "current_sign_in_at"
-    #   t.datetime "last_sign_in_at"
-    #   t.string   "current_sign_in_ip"
-    #   t.string   "last_sign_in_ip"
-    #   t.datetime "created_at"
-    #   t.datetime "updated_at"
-    #   t.string   "name"
-    # end
-
-    # add_index "users", ["email"], name: "index_users_on_email", unique: true
-    # add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   create_table "users", force: :cascade do |t|
     t.string   "nickname"
     t.string   "email"
