7e5c93dfe862e82965dc228e6dd6b21616028b37
Konr Larson <larsonkonr@gmail.com>
2015-04-03 20:47:18 -0600

    alert

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -6,7 +6,7 @@ class MessagesController < ApplicationController
     @message = @conversation.messages.build(message_params)
     @message.user_id = current_user.id
     @message.save!
-
+    PrivatePub.publish_to("/conversations/#{@conversation}/new", "alert('#{@message.body}');")
     @path = conversation_path(@conversation)
   end
 
